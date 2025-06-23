5963c3ac3fd000a6fa323b5e8d5fa3870fef3286
PQNelfox01.dev <nelfox0123@gmail.com>
2025-06-23 02:15:53 -0400

    actualizamos index.js
    Actualizacion; ahora podemos elegir que plantilla ver si la 1, 2, 3... etc...

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -2,7 +2,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const btnLogo = document.getElementById("btnLogoSNTS");
 
     btnLogo.addEventListener("click", () => {
-        // Redirige al archivo plantilla.html
-        window.location.href = "/frontend/html/plantilla.html";
+        const opcion = prompt("¿Qué plantilla deseas ver?\nEscribe 1 para la plantilla clásica\nEscribe 2 para la nueva versión con tarjetas");
+
+        if (opcion === "1") {
+            window.location.href = "/frontend/html/plantilla.html";
+        } else if (opcion === "2") {
+            window.location.href = "/frontend/html/plantilla2.html";
+        } else {
+            alert("Opción no válida. Intenta de nuevo.");
+        }
     });
 });
