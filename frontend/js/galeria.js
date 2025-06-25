ee212838d5a6175c26f5c688e5cad57a85c28206
PQNelfox01.dev <nelfox0123@gmail.com>
2025-06-25 14:51:01 -0400

    Actualización

--- frontend/js/galeria.js
+++ frontend/js/galeria.js
@@ -8,10 +8,10 @@ export function renderGaleria(jugadores) {
     card.innerHTML = `
       <img src="${j.img}" alt="${j.nombre}">
       <div class="info">
-        <p><strong>${j.nombre}</strong> - ${j.desc}</p>
-        <a href="${j.tracker}" target="_blank">Ver en RL Tracker</a>
-        ${j.streamer ? `<span class="insignia">🎥 Streamer activo</span>` : ""}
-        ${j.streamer ? `<span class="estado-stream ${j.enVivo ? 'online' : 'offline'}">${j.enVivo ? '🎥 En directo ahora' : '❌ Streamer desconectado'}</span>` : ""}
+        <p><strong>${j.nombre}</strong><br>${j.desc}</p>
+        ${j.streamer ? `<span class="insignia">🎥 Streamer del Clan</span>` : ""}
+        ${j.streamer ? `<span class="estado-stream ${j.enVivo ? 'online' : 'offline'}">${j.enVivo ? '🔴 En vivo ahora' : '⚫ Fuera de línea'}</span>` : ""}
+        <a href="${j.tracker}" target="_blank">Ver estadísticas</a>
       </div>
     `;
     contenedor.appendChild(card);
