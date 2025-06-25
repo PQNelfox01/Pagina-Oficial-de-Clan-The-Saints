8f0498421fd83ce18a1618341dd747f17f4bc0c3
PQNelfox01.dev <nelfox0123@gmail.com>
2025-06-25 14:00:40 -0400

    redireccion automatica

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -2,12 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const btnLogo = document.getElementById("btnLogoSNTS");
 
     btnLogo.addEventListener("click", () => {
-        const opcion = prompt("¿Qué plantilla deseas ver?\nEscribe 1 para la plantilla definitiva.");
-
-        if (opcion === "1") {
-            window.location.href = "/frontend/html/plantilla.html";
-        } else {
-            alert("Opción no válida. Intenta de nuevo.");
-        }
+        // Redirige automáticamente sin preguntar
+        window.location.href = "html/plantilla.html";
     });
 });
